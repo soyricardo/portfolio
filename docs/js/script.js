b9125a437f751e063eca3166c331a6f7c92f761f
Ricardo <soyricardos@hotmail.com>
2019-02-28 15:47:33 +0100

    corregido errores con imágenes

---
+++ docs/js/script.js
@@ -0,0 +1,13 @@
+// Clients - Owl Carousel
+
+$(function () {
+	$("#mis_clientes").owlCarousel({
+		items: 6,
+		autoplay: true,
+		smartSpeed: 700,
+		loop: true,
+		autoplayHoverPause:true
+
+	});
+
+});
